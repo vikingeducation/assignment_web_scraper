13817eea4e426e3783a21d91f9ffedec1268454f
telwell <me@trevorelwell.com>
2015-05-05 22:40:53 -0400

    Almost done scraping info.

---
+++ scrape.rb
@@ -0,0 +1,79 @@
+# TODO: Potential improvements. Right now dice has two types of links: regular and 
+# highlighted links. This scraper works only on the regular links. Highlighted links
+# have a url scheme of (/jobs/result) and the page, once clicked, is also much different.
+# For now, I'm going to leave it working w/ just the regular links and leave this as an
+# improvement.
+
+require 'rubygems'
+require 'mechanize'
+
+# Create a struct for the final results
+Result = Struct.new(:job_title, :company_name, :link, :location, :date, :company_id, :job_id)
+
+# Going to create a class for the Scrape Bot
+class ScrapeBot
+	def initialize
+		@agent = get_agent
+	end
+
+	def get_agent
+		agent = Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
+	end
+
+	def search(search_term)
+		page = @agent.get('http://dice.com/') do |page|
+			search_results = get_search_results(page)			# Get search results page
+			job_links = get_job_links(search_results) 		# Get the job links
+			get_job_links_info(job_links)
+		end
+	end
+
+	# Returns the first page of all search results
+	def get_search_results(page)
+		search_results = page.form_with(:action => '/jobs') do |search|
+			search.q = 'Ruby' 
+		end.submit
+		search_results
+	end
+
+	# Returns the links of all job links
+	def get_job_links(search_results)
+		job_links = search_results.links.find_all do |l| 
+			!l.href.nil? && l.href.match(/(\/jobs\/detail\/)/)
+		end
+		job_links
+	end
+
+	# Gets the relevant information from each job link
+	def get_job_links_info(job_links)
+		all_results = []
+		job_links.each do |l|
+			results_page = l.click
+			results = Result.new
+			results.link = l.href
+			# Creating a hash of all of the information and their corresponding
+			# CSS element to add to the struct.
+			attributes_info = {:job_title => '.jobTitle', :company_name => '#companyNameLink', :location => '.location', 
+													:date => '.posted', :company_id => '', :job_id => '' }
+			attributes_info.each do |attribute, css|
+				results[attribute] = results_page.parser.css(css).inner_text
+			end
+			# These two don't fit the schema from above so
+			# have to be treated a bit differently.
+			results.company_id = get_company_id(results_page)
+			results.job_id = get_job_id(results_page)
+			binding.pry
+		end
+	end
+
+	def get_company_id(results_page)
+		id = results_page.parser.css('.company-header-info').inner_html
+		id = id.match(/Dice Id : (\d*)/)
+		return "Company ID: #{id[0]}"
+	end
+
+	def get_job_id(results_page)
+		id = results_page.parser.css('.company-header-info').inner_html.match(/Position Id : (\d*)/)
+		return "Job ID: #{id[0]}"
+	end
+end
