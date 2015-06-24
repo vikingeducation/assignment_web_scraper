a0c99674dc7657d891e9610af8f9255c77354ee0
Adam <adam.kinson@gmail.com>
2015-06-23 22:44:06 -0400

    build crawler to pull data on all posts

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,10 @@ require 'mechanize'
 Result = Struct.new(:job_title, :company, :link, :location, :post_date, :company_id, :job_id)
 
 
+#class DiceScraper < Mechanize
+
+#end
+
 agent = Mechanize.new
 agent.history_added = Proc.new { sleep 0.5 }
 
@@ -26,38 +30,37 @@ page = agent.submit(search_form)
   # in div class="serp-result-content" > h3 > a
 result_links = page.search("div.serp-result-content h3 a")
 
-next_page = result_links.first
-result_page = agent.click(next_page)
+array = []
+
+result_links.each do |result|
+
+  begin
+    agent.transact do
+      result_page = agent.click(result)
+
+      job_title = result_page.at("h1#jt").text
+
+      company = result_page.at("li.employer").text.strip
+
+      link = result_page.uri.to_s
+
+      location = result_page.at("li.location").text
+
+      post_date = result_page.at("li.posted").text
+
+      company_id = result_page.body.match(/.*Dice Id : (.*)<\/div>/)[1].strip
+
+      job_id = result_page.body.match(/.*Position Id : (.*)<\/div>/)[1].strip
 
 
-# Job title
-#    -> div id="header-wrap" > h1 id="jt"
-job_title = result_page.at("h1#jt").text
+      array << Result.new(job_title, company, link, location, post_date, company_id, job_id)
 
-# Company name
-#    ^ + li class="employer"
-company = result_page.at("li.employer").text.strip
+    end
 
-# Link to posting on Dice
-#    -> current page
-link = result_page.uri.to_s
+  rescue => e
+    $stderr.puts "#{e.class}: #{e.message}"
+  end
 
-# Location
-#    ^ + li class="location"
-location = result_page.at("li.location").text
+end
 
-# Posting date (not a relative date like "x weeks ago" because that will quickly be meaningless)
-#    ^ + li class="posted" (compare to current date)
-post_date = result_page.at("li.posted").text
-
-# Company ID
-#    -> div class="company-header-info" 2nd "row"
-company_id = result_page.body.match(/.*Dice Id : (.*)<\/div>/)[1].strip
-
-# Job ID
-#    -> div class="company-header-info" 3rd "row"
-job_id = result_page.body.match(/.*Position Id : (.*)<\/div>/)[1].strip
-
-
-result_data = Result.new(job_title, company, link, location, post_date, company_id, job_id)
-p result_data
+puts array
