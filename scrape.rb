41f1098601b8fb02d2593c9475b04bb8674ad792
facingsouth <xin.d.liu@gmail.com>
2015-07-11 10:18:42 -0700

    scrape

--- scrape.rb
+++ scrape.rb
@@ -7,13 +7,41 @@ class Scraper
   end
 
   def search_dice(query, location)
-    @agent.get(formatted_url(query, location))
+    parse_page(@agent.get(formatted_url(query, location)))  
   end
 
   def formatted_url(query, location)
     "www.dice.com/jobs?q=#{query}&l=#{location}"
   end
 
+  def parse_page(page)
+    listings = page.search(".serp-result-content")
+
+    info_array = []
+    listings.each do |listing|
+      info = {}
+      info[:title] = listing.search('.dice-btn-link')[0].inner_text.strip
+      info[:link] = listing.search('.dice-btn-link')[0]["href"]
+      info[:cname] = listing.search('.dice-btn-link')[1].inner_text.strip
+      info[:cid] = get_cid(listing.search('.dice-btn-link')[1]["href"])
+      info[:loc] = listing.search('.location')[0].inner_text.strip
+      info[:date] = get_post_date(listing.search('.posted')[0].inner_text.strip)
+      info_array << info
+    end
+  end
+
+
+  def get_cid(url)
+    url.split("/")[-1]
+  end
+
+  def get_post_date(relative_time_str)
+    relative_time = parse_time(relative_time_str)
+    time = Time.now + relative_time
+  end
+
+  def parse_time(relative_time_str)
+  end
 
 end
 
