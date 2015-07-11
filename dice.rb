d74e541e238ee5dafbe978d742e7800f60fd34b0
Garrett <garrettweinert@gmail.com>
2015-07-11 10:13:21 -0700

    made mine goog_searcher

--- dice.rb
+++ dice.rb
@@ -16,17 +16,29 @@ require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
 
+def goog_searcher
+	a = Mechanize.new
 
-a = Mechanize.new
-a.get("http://www.dice.com/") do |page|
-	#submit the search fields and get result
-	search_result = page.form_with(:class => 'form-control') do |submit_query|
-		submit_query.q = "Ruby on Rails"
-		submit_query.l = "San Francisco, CA"
-	end.submit
-
-	search_result.length.each  do |link|
-		puts link.text
+	a.get('http://google.com/') do |page|
+	  search_result = page.form('f')
+	  search_result.q = 'Hello world'
+	  page = a.submit(search_result, search_result.buttons.first)
+	  # end.submit
+	  # pp page
+	  page.links.each do |link|
+	    puts link.text
+	  end
 	end
-
 end
+# a.get("http://www.dice.com/") do |page|
+# 	#submit the search fields and get result
+# 	search_result = page.form_with(:class => 'form-control') do |submit_query|
+# 		submit_query.q = "Ruby on Rails"
+# 		submit_query.l = "San Francisco, CA"
+# 	end.submit
+
+# 	search_result.length.each  do |link|
+# 		puts link.text
+# 	end
+
+# end
