12bf6de4c8156410c19b396f476b04330393305d
Alice Fung <alice.o.fung@gmail.com>
2015-07-11 13:36:36 -0400

    some comments

--- web_scraper.rb
+++ web_scraper.rb
@@ -2,22 +2,37 @@ require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
 
+require 'csv'
 
 
 agent = Mechanize.new
+
+#mechanize object waits 0.5secs after every HTML request
+agent.history_added = Proc.new { sleep 0.5 }
+
+#site we're visiting
 page = agent.get('http://www.dice.com')
 
+#where the search leads us to
 dice_search_page = "http://www.dice.com/jobs"
 
+#sends submits search input into form
 agent.get(dice_search_page) do |page|
   results = page.form_with(:name => nil) do |search|
       search.q = 'developer'
       search.l = 'New York, NY'
   end.submit
 
+  #prints out results of search page by link
   results.links.each do |link|
-    pp link.text
+    puts link.text
   end
 
   #pp page
 end
+
+#ex: CSV.open('some_csv_file.csv', 'a') do |csv|
+#      csv << #put stuff in one row
+#      csv << #put stuff in another row
+#    end
+#note: .gitignore output csv file
