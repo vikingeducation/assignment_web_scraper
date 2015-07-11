7ae074d24bbb247ebe1e2b17ef2d9c0e2f349508
alokp <alokpra@gmail.com>
2015-07-11 16:55:39 -0700

    completed basic assignment

--- create_joblist.rb
+++ create_joblist.rb
@@ -5,7 +5,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
 require 'csv'
-require 'time'
+require 'Date'
 
 class WriteJobs
 	attr_accessor :job_search_results
@@ -18,15 +18,10 @@ class WriteJobs
 	def create_arr_of_job_entries
 		@job_search_results.each_with_index do |job, i|
 			CSV.open('csv_file.csv', 'a') do |csv|
-				# csv << [title(job), co_name(job), post_link(job), location(job), post_date(job), job_id(job), company_id(job)]
 				csv << [title(job), co_name(job), post_link(job),
-						location(job), company_id(job), job_id(job)]
+						location(job), company_id(job), job_id(job), post_date(job)]
 			end
 		end
-	end
-
-	def test
-		title
 	end
 
 	def title(job)
@@ -47,42 +42,32 @@ class WriteJobs
 
 	def post_date(job)
 		posting_sched = job.css("li")[2].text
-		time = time.now
-		day = time.day
-		month = time.month
-		year = time.year
-
+		date = Date.today
 		if posting_sched.include?("day") || posting_sched.include?("days")
 			posting_day = posting_sched.match(/\d/)
-			day = day - posting_day[0].to_i
-			"Post date: #{month}/#{day}/#{year}"
+			"Post date: #{date - posting_day[0].to_i}"
 		elsif posting_sched.include?("week") || posting_sched.include?("weeks")
 			posting_week = posting_sched.match(/\d/)
-			week = week - posting_day[0].to_i
-			"Post date: #{month}/#{day}/#{year}"
+			"Post date: #{date - posting_week[0]*7}"
 		elsif posting_sched.include?("month") || posting_sched.include?("months")
-
+			posting_month = posting_sched.match(/\d/)
+			"Post date: #{date.prev_month(posting_month)}"
 		else
-			"Post date: #{month}/#{day}/#{year}"
+			"Post date: #{date}"
 		end
 	end
 
 	def company_id(job)
-		#slice off question mark
-		#take off next to last one is co ID
 		link = job.css("h3 a").first.attr('href')
 		com_id_string = link.match(/(?<=[0-9]\/)([a-zA-Z]||[0-9]).*(?=\/)/)
 		com_id_string[0]
-		# link = post_link(job)
 	end
 
 	def job_id(job)
-		#slice off question mark
-		#take off last one is job id
 		link = job.css("h3 a").first.attr('href')
 		job_id_string = link.match(/(?<=\/)([A-Z]|[0-9])+([A-Z]|[0-9])+.*(?=\?)/)
-		job_id_string[0]
-
+		exact = job_id_string[0].split("/") if job_id_string[0].include?("/")
+		exact != nil ? exact[1] : job_id_string[0]
 	end
 
 end
