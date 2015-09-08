2ac370b74ca776e9451d521d38a90dc296df6525
leedu708 <djl265@cornell.edu>
2015-09-08 11:53:40 -0400

    able to load necessary data

--- scraper.rb
+++ scraper.rb
@@ -4,8 +4,25 @@ require 'mechanize'
 require 'csv'
 
 class Scraper
+  attr_reader :page
 
   def initialize
+
     @agent = Mechanize.new
+
   end
 
+  def scrap
+
+    # skip form, go directly to results
+    @page = @agent.get("https://www.dice.com/jobs?q=&l=Roslyn%2C+NY")
+
+    # all results contain necessary information under one class
+    results = @page.search(".//div[@class='serp-results-content']")
+
+  end
+
+end
+
+# test = Scraper.new
+# pp test.scrap
