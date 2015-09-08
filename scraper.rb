0d2ed8bfc8fb4981701e85f200f4932cd7d9d205
leedu708 <djl265@cornell.edu>
2015-09-08 13:05:33 -0400

    completed assignment

--- scraper.rb
+++ scraper.rb
@@ -42,10 +42,10 @@ class Scraper
       company_name = job.at_css('li a').text.strip
       post_link = job.at_css('h3 a').attribute('href').value
       location = job.at_css('li.location').text
-      post_date = job.at_css('li.posted').text
+      post_date = get_date(job.at_css('li.posted').text)
 
       # capture every instance of characters in between '/' and '/'.  only returns the last value which is the company ID
-      company_ID = job.at_css('h3 a').attribute('href').value.scan(/([^\/]*)\//)[-1]
+      company_ID = job.at_css('h3 a').attribute('href').value.scan(/([^\/]*)\//)[-1][0]
 
       # capture all values before the first question mark
       job_ID = job.at_css('h3 a').attribute('href').value.match(/([^\/]*)\?/)[1]
@@ -58,6 +58,45 @@ class Scraper
 
   end
 
+  def get_date(post_time)
+
+    # gives value of time (e.g. "1" minute, "3" hours)
+    value = post_time.split(' ')[0].to_i
+
+    # gives type of time (e.g. 2 "weeks", 6 "hours")
+    time_type = post_time.split(' ')[1]
+
+    # returns type of time as singular (e.g. weeks --> week) for case structure
+    time_type = time_type[0..-2] if time_type[-1] == "s"
+
+    # strictly returns the date (MM/DD/YYYY)
+    case time_type.downcase
+
+      # subtract seconds ago
+      when "second"
+        Time.at(Time.now.to_i-value).strftime("%m-%d-%Y")
+
+      # subtract minutes ago
+      when "minute"
+        Time.at(Time.now.to_i-value*60).strftime("%m-%d-%Y")
+
+      # subtract hours ago
+      when "hour"
+        Time.at(Time.now.to_i-value*60*60).strftime("%m-%d-%Y")
+
+      # subtract days ago
+      when "day"
+        Time.at(Time.now.to_i-value*60*60*24).strftime("%m-%d-%Y")
+
+      # subtract weeks ago
+      when "week"
+        Time.at(Time.now.to_i-value*60*60*24*7).strftime("%m-%d-%Y")
+
+    end
+
+  end
+
+  # saves results to a CSV file
   def save_results(jobs)
 
     CSV.open('dice_job_directory.csv', 'a') do |csv|
