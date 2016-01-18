23c20736393b082264a9a23a609164e84dfab3a8
Julia Herron <julia.herron@gmail.com>
2016-01-18 15:15:37 -0800

    now we have an array of job info

--- page_getter.rb
+++ page_getter.rb
@@ -6,26 +6,46 @@ class PageGetter
 
   HOME_PAGE = 'http://www.dice.com/jobs'
 
-  attr_reader :page, :agent, :query
+  attr_reader :full_page, :agent, :search_query, :jobs
 
   def initialize
     @agent = Mechanize.new
-    @page = nil
-    @query = nil
+    @agent.history_added = Proc.new { sleep 0.5 }
+    @full_page = nil
+    @search_query = nil
+    @jobs = []
   end
 
-  def visit_url(target)
-    @page = agent.get(target)
+  def make_search_query(search_term, location)
+    @search_query = HOME_PAGE + "?q=#{search_term}" + "&l=#{location}"
   end
 
-  def make_query(search_term, location)
-    @query = HOME_PAGE + "?q=#{search_term}" + "&l=#{location}"
+
+  def create_jobs_array(target_url)
+    @full_page = agent.get(target_url)
+    all_links = @full_page.search("#serp")
+    root = all_links[0]
+    # p root
+
+    stack = []
+    stack << root
+    while node = stack.pop
+      node.children.each do |child|
+        @jobs << child.attributes
+        @jobs << child.children
+        stack << child
+      end
+    end
+  end
+
+
+  def get_job_info
+
   end
 
 end
 
-
 m = PageGetter.new
-m.make_query("ruby", "francisco")
-m.visit_url(m.query)
-p m.page
+m.make_search_query("ruby", "francisco")
+m.create_jobs_array(m.search_query)
+pp m.jobs
