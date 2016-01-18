9a4e499dd00a622ff985f3fbaded47afae7dc9e1
Andrew Baik <and.baik@gmail.com>
2016-01-18 14:14:00 -0500

    added pseudocode for scraping structure of dice.com

---
+++ scraper.rb
@@ -0,0 +1,16 @@
+# job title, keywords input field id='search-field-keyword'
+# location id='search-field-location'
+# button type='submit', text='Find Tech Jobs'
+
+# Search URL
+#
+# https://www.dice.com/jobs?q=ruby+on+rails&l=
+
+# grab everything under each div class='serp-result-content'
+# broken into
+# h3
+# div class='shortdesc'
+# ul class='list-inline details'
+# => li class='employer'
+# => li class='location'
+# => li class='posted'
