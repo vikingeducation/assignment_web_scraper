3533b1724e204c196add5649fe90c762092e97ea
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-18 18:05:04 -0500

    continue working on scraper

--- scraper.rb
+++ scraper.rb
@@ -8,23 +8,46 @@ class Scraper
 
   def initialize
     @mech = Mechanize.new
+    @mech.user_agent_alias = 'Mac Safari'
+    @page = nil
     # this gives your Mechanize object
     # an 0.5 second wait time after every HTML request
     # Don't forget it!!!
     @mech.history_added = Proc.new { sleep 0.5 }
-
   end
 
   def scrape_jobs
-    results = @mech.get('https://www.dice.com/jobs?q=Ruby+on+Rails&l=New+York%2C+NY').search('.serp-result-content')
-
+    @page = @mech.get('https://www.dice.com/jobs?q=Ruby+on+Rails&l=New+York%2C+NY')
+    results = @page.search('#search-results-control .col-md-9 #serp .serp-result-content')
+    puts results.length
     results.each do |job|
-      # current_job = Job.new
-      # current_job.title = link.text
-      puts job
+      job_title = job.at('h3 a').attributes["title"].value
+      job_shortdesc = job.css('div.shortdesc').text.strip
+      employer = job.css('li.employer span.hidden-md a').text.strip
+      job_link = job.at('h3 a').attributes["href"].value
+      location = job.css('li.location').text.strip
+      rel_time_arr = job.css('li.posted').text.split(" ")
+      now = Time.now
+      time_num = rel_time_arr[0].to_i
+      time_string = rel_time_arr[1]
+
+      time_hash = { year: 365*24*60*60, years: 365*24*60*60,
+                    week: 7*24*60*60, weeks: 7*24*60*60,
+                    month: 30*24*60*60, months: 30*24*60*60,
+                    day: 24*60*60, days: 24*60*60,
+                    hour: 60*60, hours: 60*60,
+                    minute: 60, minutes: 60,
+                    second: 1, seconds: 1 }
+
+
+      actual_time = now - time_num*time_hash[time_string.downcase.to_sym]
+
+      
+
+      current_job = Job.new
+      p employer
       puts
     end
-    pp search
   end
 
 end
