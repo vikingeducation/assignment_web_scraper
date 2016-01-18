e4ff5ff430e42ecedaccbc6f06856f181be27a76
Andrew Baik <and.baik@gmail.com>
2016-01-18 15:02:11 -0500

    working on parsing nokogiri data

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
 
+Job = Struct.new(:title, :descript, :employer, :location, :posted)
+
 class Scraper
 
   def initialize
@@ -10,10 +12,18 @@ class Scraper
     # an 0.5 second wait time after every HTML request
     # Don't forget it!!!
     @mech.history_added = Proc.new { sleep 0.5 }
+
   end
 
   def scrape_jobs
-    search = @mech.get('https://www.dice.com/jobs?q=Ruby+on+Rails&l=New+York%2C+NY').search(".serp-result-content")
+    results = @mech.get('https://www.dice.com/jobs?q=Ruby+on+Rails&l=New+York%2C+NY').search('.serp-result-content')
+
+    results.each do |job|
+      # current_job = Job.new
+      # current_job.title = link.text
+      puts job
+      puts
+    end
     pp search
   end
 
@@ -34,7 +44,7 @@ scraper.scrape_jobs
 
 # grab everything under each div class='serp-result-content'
 # broken into
-# h3
+# h3 (grab a tag with title attribute)
 # div class='shortdesc'
 # ul class='list-inline details'
 # => li class='employer'
