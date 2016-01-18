02b2bcfc8b6f35773b1f5bf350fd76ccf8478012
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-18 14:37:55 -0500

    start building scraper

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,28 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'mechanize'
+
+class Scraper
+
+  def initialize
+    @mech = Mechanize.new
+    # this gives your Mechanize object
+    # an 0.5 second wait time after every HTML request
+    # Don't forget it!!!
+    @mech.history_added = Proc.new { sleep 0.5 }
+  end
+
+  def scrape_jobs
+    search = @mech.get('https://www.dice.com/jobs?q=Ruby+on+Rails&l=New+York%2C+NY').search(".serp-result-content")
+    pp search
+  end
+
+end
+
+scraper = Scraper.new
+
+scraper.scrape_jobs
+
 # job title, keywords input field id='search-field-keyword'
 # location id='search-field-location'
 # button type='submit', text='Find Tech Jobs'
@@ -5,6 +30,7 @@
 # Search URL
 #
 # https://www.dice.com/jobs?q=ruby+on+rails&l=
+# https://www.dice.com/jobs?q=Ruby+on+Rails
 
 # grab everything under each div class='serp-result-content'
 # broken into
@@ -14,3 +40,4 @@
 # => li class='employer'
 # => li class='location'
 # => li class='posted'
+# Filters are JavaScript only
