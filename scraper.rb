aadb8a4fdeecf6eac23401fb46d634f469d3ee90
Kit Langton <kitlangton@gmail.com>
2016-01-18 14:37:39 -0500

    YAY

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,13 @@
 require 'nokogiri'
 require 'mechanize'
+require 'byebug'
 
-agent = Mechanize.new
-agent.get("https://www.dice.com")
+address = "https://www.dice.com/jobs?q=rails"
+agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
+page = agent.get(address)
+jobs = page.css(".col-md-9 > #serp > .serp-result-content")
+
+jobs.each do |job|
+  puts job.at("h3 a").attributes["title"].value
+end
+
