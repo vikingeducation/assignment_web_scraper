cdcf95f41ef56013ee1ef83c4857f0d04d4d1e96
Julia Herron <julia.herron@gmail.com>
2016-01-18 17:58:34 -0800

    working on scraping all listing from the page, got visit_all_links to work

--- page_getter.rb
+++ page_getter.rb
@@ -36,44 +36,85 @@ class PageGetter
         stack << child
       end
     end
+    @jobs.compact
   end
 
 
   def get_job_info
+    listings = []  # all 30 on the page
+    # [listing0, listing1, listing2]
     listing = {}
     i = 0
 
+    (0..29).each_with_index do |test, index|
+      position_number = "position" + index.to_s
+      company_number = "company" + index.to_s
+      current_listing = ""
 
-    # until i == 29 do |jobs|
-        @jobs.each_with_index do |info_block, index|
-          if info_block.nil? 
-            print "Nothing to evaluate."
-          elsif !info_block.is_a? Hash
-            print "Nothing to evaluate."
-          elsif info_block['id'].nil?
-              print "Nothing to evaluate."
-          elsif info_block['id'].value.nil?
-            print "Nothing to evaluate."
-          else
-            if info_block['id'].value == 'position0'
-              listing['job_title'] = info_block['title'].value
-              listing['link'] = info_block['href'].value
-              listing['job_id'] = info_block['href'].value
-            end
-            if info_block['id'].value == 'company0'
-              listing['company_id'] = info_block['href'].value
-              listing['company_name'] = @jobs[index + 1].text
-              listing['company_location'] = @jobs[index + 5].text
-              listing['posting_date'] = @jobs[index + 9].text
-            end
+      @jobs.each_with_index do |info_block, index|
+        if info_block.nil? 
+          next
+        elsif !info_block.is_a? Hash
+          next
+        elsif info_block['id'].nil?
+          next
+        elsif info_block['id'].value.nil?
+          next
+        else
+          if info_block['id'].value == position_number
+            # print "position0 index: #{index}"
+            listing['job_title'] = info_block['title'].value
+            listing['link'] = info_block['href'].value
+            listing['job_id'] = info_block['href'].value
+          end
+          if info_block['id'].value == company_number
+            # print "company0 index: #{index}"
+            listing['company_id'] = info_block['href'].value
+            listing['company_name'] = @jobs[index + 1].text
+            # listing['company_location'] = @jobs[index + 5].text
+            # listing['posting_date'] = @jobs[index + 9].text
           end
         end
-        print listing
+        # listings[index] = listing
+      end
+      # listings << listing
+      # listings = ["listing"=> data, "listing"=> new_data]
+    end
+    print listing
+  end
+
+  def visit_all_links
+    # go to page with links
+    @full_page = agent.get(@search_query)
+    # all_links = @full_page.search("#serp")
+
+    count = 0
+    queue = []
+    # get list of links to visit
+    @full_page.links.each do |link|
+      queue << link
+      count += 1
+    end
+    puts "We need to visit #{count} links."
+
+    visited = 0
+    # go to each link in order, directly from the previous one
+    queue.each do |link|
+      agent.get(link)
+      # call another function to scrape the new page
+      visited += 1
+      break if visited == 4
+    end
+    puts "We have visited #{visited} links."
+    # stop after no more links
+
   end
 
 end
 
+
 m = PageGetter.new
 m.make_search_query("ruby", "francisco")
+# m.visit_all_links
 m.create_jobs_array(m.search_query)
 m.get_job_info
