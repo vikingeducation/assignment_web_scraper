e9bd2c3679a30bf23b1559674b99d79b39c9c8e4
Morgan Martin <morganm5201@gmail.com>
2016-07-19 18:20:34 -0400

    add csv creation

--- dice_scraper.rb
+++ dice_scraper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
+require 'csv'
 
 class Scraper
 
@@ -23,11 +24,34 @@ class Scraper
     links = get_links
     links.map do |url|
       @page = @agent.get(url)
-      @page.parser.css("h1.jobTitle").children.first.text
-      @page.parser.css("li.employer a").children.first.text
-      url
-      p @page.parser.css("li.location").children.first.text
+      {
+        jobtitle: @page.parser.css("h1.jobTitle").children.first.text,
+        employer: @page.parser.css("li.employer a").children.first.text,
+        postdate: convert_time(@page.parser.css('ul.details').at('li:contains("Posted")').children.first.text),
+        url: url,
+        location: @page.parser.css("li.location").children.first.text,
+        companyid: @page.parser.css("div.col-md-12").at('div:contains("Dice")').children.first.text,
+        jobid: @page.parser.css('div.col-md-12').at('div:contains("Position Id")').children.first.text
+      }
     end
+  end
+
+  def write_to_csv(array)
+    values = array.map do |hash|
+      hash.map { |k,v| v }
+    end
+    values
+    CSV.open('csv_file.csv', 'a') do |csv|
+      values.each { |val| csv << val }
+    end
+  end
+
+  def convert_time(string)
+    units = {hours: 1, hour: 1, day: 24,
+             days: 24, week: 168, weeks: 168}
+    num = string.split[1].to_i
+    unit = string.split[2].to_sym
+    (Time.now - units[unit] * num).to_s
   end
 
   def get_links
@@ -40,4 +64,4 @@ end
 
 searcher = Scraper.new("developer", "raleigh, nc")
 searcher.submit_form
-searcher.build_job_hash
+p searcher.write_to_csv(searcher.build_job_hash)
