61c42cef21a5369c33279be434e8fd90cea1c496
Graham Turner <turnertgraham@gmail.com>
2016-07-19 17:27:33 -0400

    building job hash, on a roll

--- dice_scraper.rb
+++ dice_scraper.rb
@@ -2,42 +2,42 @@ require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
 
-agent = Mechanize.new
+class Scraper
 
-agent.history_added = Proc.new { sleep 0.5 }
+  def initialize(job, location)
+    @job = job
+    @location = location
+    @agent = Mechanize.new
+    @agent.history_added = Proc.new { sleep 0.5 }
+    @page = @agent.get('http://www.dice.com')
+  end
 
-page = agent.get('http://www.dice.com')
+  def submit_form
+    form = @page.form_with(:id => 'search-form')
+    form.q = 'developer'
+    form.l = 'raleigh, nc'
+    @page = @agent.submit(form)
+  end
 
-form = page.form_with(:id => 'search-form')
+  def build_job_hash
+    links = get_links
+    links.map do |url|
+      @page = @agent.get(url)
+      @page.parser.css("h1.jobTitle").children.first.text
+      @page.parser.css("li.employer a").children.first.text
+      url
+      p @page.parser.css("li.location").children.first.text
+    end
+  end
 
-form.q = 'developer'
+  def get_links
+    links = @page.parser.css("a[id]").select{ |a| a['id'] =~ /\Aposition/ }
+    links = links.map { |link_ob| link_ob.attribute_nodes[3] }.compact!
+    links.map { |link_atr| link_atr.value }
+  end
 
-form.l = 'raleigh, nc'
+end
 
-page = agent.submit(form)
-# links = page.links_with(id: /\Aposition/)
-
-divs = page.parser.css("a[id]").select{ |a| a['id'] =~ /\Aposition/ } #[0].attribute_nodes[3].value
-
-
-divs.map! { |div| div.attribute_nodes[3] }.compact!
-
-links = divs.map { |div| div.value }
-
-# links.each do |link|
-#   page = link.click
-# end
-pp links
-#jobids = [], [], [], [], [], [], []
-
-#posting = { jobtitle: nil, companyname: nil, location: nil, postingdate: nil,
-#            companyid: nil, jobid: nil }
-
-# postings = divs.map do |div|
-#   {
-#   jobtitle: div.css("a[id]").select{ |a| a['id'] =~ /\Aposition/ }[0].attribute_nodes[1].value,
-#   companyname: div.css("span.hidden-xs")[0].attribute_nodes[1].value,
-#   location: div.css("li.location")[0].children[1].text,
-#   postingdate: div.css("li.location")[0].children[1].text
-#   }
-# end
+searcher = Scraper.new("developer", "raleigh, nc")
+searcher.submit_form
+searcher.build_job_hash
