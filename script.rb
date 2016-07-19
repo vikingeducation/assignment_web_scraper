f3ea0f6c8c4f2bca2fbb4f31c30c3608c20e5be0
David Jiang <davidjgrey@gmail>
2016-07-19 15:23:34 -0700

    getting company info

--- script.rb
+++ script.rb
@@ -11,6 +11,7 @@ class DiceScraper
     @page = agent.get('http://www.dice.com/')
     @query = query
     @location = location
+    @jobs =[]
     build
   end
   
@@ -59,17 +60,36 @@ class DiceScraper
 
   #inputs a link and outputs company_arr
   def get_company_info(link)
+    info = {}
     company_page =  parse_page(@agent.get(link))
 
     noko_job = get_elements(company_page, ".jobTitle").children.to_s
+    noko_job = noko_job.gsub("  ", " ")
     noko_company = get_elements(company_page, ".employer .dice-btn-link").children.to_s
     noko_location = get_elements(company_page, ".list-inline .location").children.to_s
-    noko_date = get_elements(company_page, ".posted").children.to_s
     noko_company_id = get_elements(company_page, ".company-header-info .col-md-12").children[5].to_s
     noko_position_id = get_elements(company_page, ".company-header-info .col-md-12").children[6].to_s
+    all_info = get_elements(company_page, "title").children.to_s
+    date = all_info.match(/\d\d-\d\d-\d\d\d\d/)[0]
     binding.pry
-    
+    company_id = noko_company_id.match(/: ([-\da-zA-Z]*)/).captures[0]
+    position_id = noko_position_id.match(/: ([-\da-zA-Z]*)/).captures[0]
+    #binding.pry
+    info[:title] = noko_job
+    info[:company] = noko_company
+    info[:location] = noko_location
+    info[:date] = date
+    info[:company_id] = company_id
+    info[:position_id] = position_id
+    info
   end
+
+  def get_all_company_info(link_array)
+    link_array.each do |link|
+      @jobs << get_company_info(link)
+    end
+  end
+
 end
 
 d = DiceScraper.new("Web Developer","Hanover, NH")
@@ -77,6 +97,7 @@ d = DiceScraper.new("Web Developer","Hanover, NH")
 arr = d.find_elements_and_return_links("div#search-results-experiment h3 .dice-btn-link")
 
 p d.get_company_info(arr[0])
+#p d.get_all_company_info(arr)
 
 #test to recieve array of links
 #p d.find_elements_and_return_links("div#search-results-experiment h3 .dice-btn-link")
