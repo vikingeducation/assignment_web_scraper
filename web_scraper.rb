6cf55556ca9fc89001f81319371dbfe17ac05e21
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-19 11:42:26 -0700

    Starting to get some results

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
+require 'csv'
 
 # Instantiate a new Mechanize
 scraper = Mechanize.new
@@ -9,13 +10,19 @@ scraper.history_added = Proc.new { sleep 0.5 }
 
 # Grab and parse our page in one step
 # like we did with Nokogiri and open-uri
-page = scraper.get('https://www.dice.com/jobs/advancedResult.html?for_one=Ruby&for_all=&for_exact=&for_none=&for_jt=&for_com=&for_loc=New+York%2C+NY&sort=relevance&limit=50&radius=0&limit=50&radius=0')
+page = scraper.get('https://www.dice.com/jobs/advancedResult.html?for_one=Ruby&for_all=&for_exact=&for_none=&for_jt=&for_com=&for_loc=New+York%2C+NY&sort=relevance&limit=50&radius=0')
 
-pp page.search('.serp-result-content').count 
+
+
+search_results = page.search('.serp-result-content')
+p search_results.count
+
+pp search_results[1].css('h3').text.strip
+pp search_results[1].css("[id*='company']").text
+
+
 # Grab the form of class="f" from the page
 # job_div = page.div(:class => "serp-result-content")
-
-pp job_div
 
 # Grab the form by ID or action
 # another_form = page.form_with(:id => "some-id")
