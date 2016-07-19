f0afb6120614de2be304b09c8e56ad86dea9c722
Alex Lach <alexglach@gmail.com>
2016-07-19 13:27:06 -0400

    not a lot of progress

--- web_scraper.rb
+++ web_scraper.rb
@@ -3,13 +3,34 @@ require 'nokogiri'
 
 class Scraper
 
-  attr_reader :page
+  attr_reader :page, :job_title, :job_location
 
   def initialize
-  scraper = Mechanize.new
-  scraper.history_added = Proc.new { sleep 0.5 }
-  @page = scraper.get("http://www.dice.com")
+    scraper = Mechanize.new
+    scraper.history_added = Proc.new { sleep 0.5 }
+    page = scraper.get("http://www.dice.com")
+    job_title = page.form_with(:id => "search-field-keyword")
+    job_location = page.form_with(:id => "search-field-location")
+    search_button = page.form_with(:class => "btn btn-primary btn-lg btn-block")
+
+    # job_title.q = "ruby"
+    # job_location.q = "Boston, MA"
+    # scraper.click(search_button)
   end
 
 
+
+  
+
+
 end
+
+
+
+
+
+
+
+
+
+  # https://www.dice.com/jobs?q=ruby&l=Boston%2C+MA
