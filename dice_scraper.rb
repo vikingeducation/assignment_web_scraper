390f406e70c88d002ee2530c8408bf55fdc081f9
Hannah Squier <hannahsquier@gmail.com>
2016-07-19 16:00:13 -0700

    finalize create listings method

--- dice_scraper.rb
+++ dice_scraper.rb
@@ -1,14 +1,18 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
+require 'time'
 
 JobListing = Struct.new(:title, :company, :link, :location, :date, :dice_id, :position_id)
 
-class JobSearcher
+  UNITS_TO_SECONDS = { minutes: 60,
+                        hours: 3600,
+                        days: 3600*24,
+                        weeks: 3600*24*7,
+                        months: 3600*24*30,
+                        years: 3600*24*365 }
 
-  CONVERSION = {
-    
-  }
+class DiceScraper
 
   attr_reader :job_page
 
@@ -37,7 +41,6 @@ class JobSearcher
     job_links = []
     page_number = 1
     until current_page.body.include?("404 - The page you're looking for couldn't be found or it may have expired.")
-
       job_links += current_page.links_with(:href => /jobs\/detail/)
       page_number += 1
       current_page = @agent.get(get_other_pages(page_number))
@@ -46,31 +49,33 @@ class JobSearcher
   end
 
   def create_listings
-    # listings_array = []
-    # get_all_links.each do |link|
-    #   current_page = link.click
-    #   title = current_page.search("h1.jobTitle").text
-        # company = current_page.search("li.employer a").text
-        #link = current_page.uri.to_s
-        #location = current_page.search("li.location").text
-        #dice_id = current_page.search("div.company-header-info div").text.match(/Dice Id : (.*?)\n/)[1]
-        #company_id = current_page.search("div.company-header-info div").text.match(/Position Id : (.*?)\n/)[1]
-        #posted = current_page.search("li.posted").text
-    # end
-    links = get_all_links
-    links[0].click.search("li.posted").text
-    
-
+    listings_array = []
+    get_all_links.each do |link|
+      current_page = link.click
+      title = current_page.search("h1.jobTitle").text
+      company = current_page.search("li.employer a").text
+      link = current_page.uri.to_s
+      location = current_page.search("li.location").text
+      dice_id = current_page.search("div.company-header-info div").text.match(/Dice Id : (.*?)\n/)[1]
+      position_id = current_page.search("div.company-header-info div").text.match(/Position Id : (.*?)\n/)[1]
+      date_string = current_page.search("li.posted").text
+      date = datestamp(date_string)
+      listings_array << [title, company, link, location, date, dice_id, position_id]
+    end
+    listings_array
   end
 
   def datestamp(posted_string)
-    posted_array = posted.split(" ")
+    integer = posted_string.split(" ")[1].to_i
+    time_unit = posted_string.split(" ")[2]
+    time_unit << "s" if time_unit[-1] != "s"
+    seconds_ago = UNITS_TO_SECONDS[time_unit.to_sym]*integer
+    posted_date = Time.now - seconds_ago
   end
-
 end
 
-j = JobSearcher.new("Software Engineer", "Boise, ID")
+j = DiceScraper.new("Software Engineer", "Fresno, CA")
 #p j.get_jobs_page.uri.to_s
 # p p.get_all_jobs
 
-p j.create_listings
+p (j.create_listings)
