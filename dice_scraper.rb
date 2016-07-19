c0acd5eb22f7afea82807afe3c082111f14d28d7
Mike Lee <asackofwheat@gmail.com>
2016-07-19 19:26:34 -0400

    it works!

--- dice_scraper.rb
+++ dice_scraper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
 require 'time'
+require 'csv' 
 
 JobListing = Struct.new(:title, :company, :link, :location, :date, :dice_id, :position_id)
 
@@ -52,17 +53,55 @@ class DiceScraper
     listings_array = []
     get_all_links.each do |link|
       current_page = link.click
-      title = current_page.search("h1.jobTitle").text
-      company = current_page.search("li.employer a").text
-      link = current_page.uri.to_s
-      location = current_page.search("li.location").text
-      dice_id = current_page.search("div.company-header-info div").text.match(/Dice Id : (.*?)\n/)[1]
-      position_id = current_page.search("div.company-header-info div").text.match(/Position Id : (.*?)\n/)[1]
-      date_string = current_page.search("li.posted").text
-      date = datestamp(date_string)
-      listings_array << [title, company, link, location, date, dice_id, position_id]
+      listings_array << [ title(current_page),
+                          company(current_page),
+                          link(current_page),
+                          location(current_page),
+                          date(current_page),
+                          dice_id(current_page),
+                          position_id(current_page)]
     end
     listings_array
+  end
+
+  def title(page)
+    return "" unless page.search("h1.jobTitle")
+    page.search("h1.jobTitle").text
+  end
+
+  def company(page)
+    return "" unless page.search("li.employer a")
+    page.search("li.employer a").text
+  end
+
+  def link(page)
+    return "" unless page.uri
+    page.uri.to_s
+  end
+
+  def location(page)
+    return "" unless page.search("li.location")
+    page.search("li.location").text
+  end
+
+  def dice_id(page)
+    return "" unless page.search("div.company-header-info div")
+    return "" unless page.search("div.company-header-info div").text
+    return "" unless page.search("div.company-header-info div").text.match(/Dice Id : (.*?)\n/)
+    page.search("div.company-header-info div").text.match(/Dice Id : (.*?)\n/)[1]
+  end
+
+  def position_id(page)
+    return "" unless page.search("div.company-header-info div")
+    return "" unless page.search("div.company-header-info div").text
+    return "" unless page.search("div.company-header-info div").text.match(/Position Id : (.*?)\n/)
+    page.search("div.company-header-info div").text.match(/Position Id : (.*?)\n/)[1]
+  end
+
+  def date(page)
+    return "" unless page.search("li.posted")
+    date_string = page.search("li.posted").text
+    datestamp(date_string)
   end
 
   def datestamp(posted_string)
@@ -72,10 +111,18 @@ class DiceScraper
     seconds_ago = UNITS_TO_SECONDS[time_unit.to_sym]*integer
     posted_date = Time.now - seconds_ago
   end
+
+  def create_csv(file_name)
+    CSV.open(file_name, 'w') do |csv|
+      csv << ["Title", "Company", "Link", "Location", "Date", "Dice ID", "Position ID"]
+      create_listings.each  {|listing| csv << listing}
+    end
+  end
+
 end
 
 j = DiceScraper.new("Software Engineer", "Fresno, CA")
 #p j.get_jobs_page.uri.to_s
 # p p.get_all_jobs
 
-p (j.create_listings)
+p (j.create_csv("listings.csv"))
