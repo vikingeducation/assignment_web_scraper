99a8a0675985a2589f956df08a016accfbbd3dba
Graham Turner <turnertgraham@gmail.com>
2016-07-19 19:00:36 -0400

    refactored build_job_hash

--- dice_scraper.rb
+++ dice_scraper.rb
@@ -23,16 +23,59 @@ class Scraper
   def build_job_hash
     links = get_links
     links.map do |url|
+      hash = {}
       @page = @agent.get(url)
-      {
-        jobtitle: @page.parser.css("h1.jobTitle").children.first.text,
-        employer: @page.parser.css("li.employer a").children.first.text,
-        postdate: convert_time(@page.parser.css('ul.details').at('li:contains("Posted")').children.first.text),
-        url: url,
-        location: @page.parser.css("li.location").children.first.text,
-        companyid: @page.parser.css("div.col-md-12").at('div:contains("Dice")').children.first.text,
-        jobid: @page.parser.css('div.col-md-12').at('div:contains("Position Id")').children.first.text
-      }
+      add_attributes(hash, url)
+    end
+  end
+
+  def add_attributes(hash, url)
+    attr_array = %w(jobtitle employer postdate url location companyid jobid)
+    attr_array.each do |title|
+      method = "add_#{title}".to_sym
+      send(method, hash) unless title == "url"
+      send(method, hash, url) if title == "url"
+    end
+    hash
+  end
+
+  def add_jobtitle(hash)
+    if node = @page.parser.css("h1.jobTitle")
+      hash[:jobtitle] = node.children.first.text
+    end
+  end
+
+  def add_employer(hash)
+    if node = @page.parser.css("li.employer a")
+      hash[:employer] = node.children.first.text
+    end
+  end
+
+  def add_postdate(hash)
+    if node = @page.parser.css('ul.details').at('li:contains("Posted")')
+      hash[:postdate] = convert_time(node.children.first.text)
+    end
+  end
+
+  def add_url(hash, url)
+    hash[:url] = url
+  end
+
+  def add_location(hash)
+    if node = @page.parser.css("li.location")
+      hash[:location] = node.children.first.text
+    end
+  end
+
+  def add_companyid(hash)
+    if node = @page.parser.css("div.col-md-12").at('div:contains("Dice")')
+      hash[:companyid] = node.children.first.text
+    end
+  end
+
+  def add_jobid(hash)
+    if node = @page.parser.css('div.col-md-12').at('div:contains("Position Id")')
+      hash[:jobid] = node.children.first.text
     end
   end
 
@@ -40,7 +83,6 @@ class Scraper
     values = array.map do |hash|
       hash.map { |k,v| v }
     end
-    values
     CSV.open('csv_file.csv', 'a') do |csv|
       values.each { |val| csv << val }
     end
@@ -61,7 +103,15 @@ class Scraper
   end
 
 end
-
+# {
+#   jobtitle: @page.parser.css("h1.jobTitle").children.first.text,
+#   employer: @page.parser.css("li.employer a").children.first.text,
+#   postdate: convert_time(@page.parser.css('ul.details').at('li:contains("Posted")').children.first.text),
+#   url: url,
+#   location: @page.parser.css("li.location").children.first.text,
+#   companyid: @page.parser.css("div.col-md-12").at('div:contains("Dice")').children.first.text,
+#   jobid: @page.parser.css('div.col-md-12').at('div:contains("Position Id")').children.first.text
+# }
 searcher = Scraper.new("developer", "raleigh, nc")
 searcher.submit_form
-p searcher.write_to_csv(searcher.build_job_hash)
+searcher.write_to_csv(searcher.build_job_hash)
