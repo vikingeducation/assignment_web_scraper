163ac2c0934460a491e317f9902e7897915323a3
Leo Ahnn <leoahnn@gmail.com>
2016-07-19 14:17:06 -0400

    working on scrapping jobs

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,14 +9,29 @@ scraper = Mechanize.new
 # p page.links[0].
 
 #job results
-p page = scraper.get('https://www.dice.com/jobs?q=web+developer&l=San+Jose\%2C+CA')
+page = scraper.get('https://www.dice.com/jobs?q=web+developer&l=San+Jose\%2C+CA')
 
-# results = page.css('div#serp-result-content')
+results = page.css('div.serp-result-content')
+str = ""
+results.map do |outter_div|
+  str += outter_div.css('h3').css('a').text.strip
+  str += "\n"
+  str += outter_div.css('div.shortdesc').text.strip
+  str += "\n"
+  details = outter_div.css('ul')
+  str += details.css('li.employer').css('span.hidden-md hidden-sm hidden-lg visible-xs').css('a').text.strip
+  str += "\n"
+  str += details.css('li.location').text.strip
+  # str += "\n"
+  # str += details.css('li.posted').css('span.icon-calendar-2').text.strip
+  str += "\n"
+end
 
-# results.each do |result|
-#   p result
-# end
+puts str
 
+# h3 includes a link that has the job title
+# div class "shortdesc" contains a short description
+# ul class "list-inline details" contains li's "employer", "location", "posted"
 
 
 
