53c4f6e30c07ef8d06d6d9c48d414208fd0bb879
cjvirtucio87 <cjv287@gmail.com>
2016-07-19 10:51:57 -0700

    Trying to scrape job results.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,21 @@ require 'mechanize'
 
 scraper = Mechanize.new
 
-page = scraper.get('https://www.dice.com')
-puts page
+
+# page = scraper.get('https://www.dice.com')
+
+# p page.links[0].
+
+#job results
+p page = scraper.get('https://www.dice.com/jobs?q=web+developer&l=San+Jose\%2C+CA')
+
+# results = page.css('div#serp-result-content')
+
+# results.each do |result|
+#   p result
+# end
+
+
 
 
 scraper.history_added = Proc.new { sleep 0.5 }
