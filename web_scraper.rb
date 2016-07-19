4397fafaf583852e836c8b2748b028a9808204a6
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-19 12:53:27 -0500

    progress in scraper

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,36 +1,21 @@
 require 'mechanize'
 require 'nokogiri'
 
-class Scraper
+# class Scraper
 
-  attr_reader :page, :job_title, :job_location
 
-  def initialize
-    scraper = Mechanize.new
-    scraper.history_added = Proc.new { sleep 0.5 }
-    page = scraper.get("http://www.dice.com")
-    job_title = page.form_with(:id => "search-field-keyword")
-    job_location = page.form_with(:id => "search-field-location")
-    search_button = page.form_with(:class => "btn btn-primary btn-lg btn-block")
-
-    # job_title.q = "ruby"
-    # job_location.q = "Boston, MA"
-    # scraper.click(search_button)
-  end
 
 
 
   
 
 
-end
+# end
 
-
-
-
-
-
-
-
-
-  # https://www.dice.com/jobs?q=ruby&l=Boston%2C+MA
+s = Mechanize.new
+page = s.get("http://www.dice.com")
+job_search = page.form_with(:id => "search-form")
+job_search.q = "ruby"
+job_search.l = "Boston"
+page = s.submit(job_search)
+jobs = page.field_with(:id => "serp")
