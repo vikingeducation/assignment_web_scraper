38f4559cef5faa9bb6491a0fc53a4c341804b420
“Matthew <matthew.hinea@gmail.com>
2016-07-19 19:10:44 -0400

    refactoring to object orientation

--- web_scraper.rb
+++ web_scraper.rb
@@ -3,76 +3,58 @@ require 'bundler/setup'
 require 'mechanize'
 require 'csv'
 
-def date_parser(date)
-  # match for minutes, hours, days, or weeks
-  # match for the number
-  # format properly to be subtracted from Time.now
-  units = date.match(/(moments|minutes|hours|days|weeks|months)/)[0]
-  number = date.match(/\d+/)[0].to_i
-  result = case units
-  when "moments"
-    0
-  when "minutes"
-    number * 60
-  when "hours"
-    number * 3600
-  when "days"
-    number * 86_400
-  when "weeks"
-    number * 86_400 * 7
-  when "months"
-    number * 86_400 * 30
+
+class DiceScraper 
+
+  def initialize 
+    @scraper = Mechanize.new
+    @scraper.history_added = Proc.new { sleep 0.5 }
   end
 
-  "Posted around #{Time.now - result}"
-end
-# Instantiate a new Mechanize
-scraper = Mechanize.new
+  def date_parser(date)
+    units = date.match(/(moment|minute|hour|day|week|month)/)[0]
+    number = date.match(/\d+/)[0].to_i
+    result = case units
+    when "moment"
+      0
+    when "minute"
+      number * 60
+    when "hour"
+      number * 3600
+    when "day"
+      number * 86_400
+    when "week"
+      number * 86_400 * 7
+    when "month"
+      number * 86_400 * 30
+    end
 
-scraper.history_added = Proc.new { sleep 0.5 }
-
-# Grab and parse our page in one step
-# like we did with Nokogiri and open-uri
-page = scraper.get('https://www.dice.com/jobs/advancedResult.html?for_one=Ruby&for_all=&for_exact=&for_none=&for_jt=&for_com=&for_loc=New+York%2C+NY&sort=relevance&limit=50&radius=0')
-
-
-search_results = page.search('.serp-result-content')
-p search_results.count
-
-
-i=0
-until (i == search_results.size/2)
-
-  pp search_results[i].css('h3').text.strip
-  pp search_results[i].css("[id*='company']")[1].text
-  p link = search_results[i].css('a[id*="position"]').map { |link| link['href'] }[0]
-  pp search_results[i].css("li.location").text
-  pp date_parser(search_results[i].css("li.posted").text)
-  link = search_results[i].css('a[id*="position"]')[0]
-
-  posting_page = scraper.get(search_results[i].css('a[id*="position"]').map { |link| link['href'] }[0])
-  pp dice_id = posting_page.search('.company-header-info').css('div')[3].text.strip
-  pp position_id = posting_page.search('.company-header-info').css('div')[5].text.strip
-  i+=1
-end
+    "Posted around #{Time.now - result}"
+  end
 
 
+  def scrape(url)  
 
+    page = @scraper.get('https://www.dice.com/jobs/advancedResult.html?for_one=Ruby&for_all=&for_exact=&for_none=&for_jt=&for_com=&for_loc=New+York%2C+NY&sort=relevance&limit=50&radius=0')
 
+    search_results = page.search('.serp-result-content')
 
+    i=0
+    until (i == search_results.size/2)
 
-# Grab the form of class="f" from the page
-# job_div = page.div(:class => "serp-result-content")
+      pp search_results[i].css('h3').text.strip
+      pp search_results[i].css("[id*='company']")[1].text
+      p link = search_results[i].css('a[id*="position"]').map { |link| link['href'] }[0]
+      pp search_results[i].css("li.location").text
+      pp date_parser(search_results[i].css("li.posted").text)
+      link = search_results[i].css('a[id*="position"]')[0]
 
-# Grab the form by ID or action
-# another_form = page.form_with(:id => "some-id")
-# another_form = page.form_with(:action => "/some_path")
+      posting_page = @scraper.get(search_results[i].css('a[id*="position"]').map { |link| link['href'] }[0])
+      pp position_id = posting_page.search('.company-header-info').css('[text()*="Position Id"]').text.strip
+      pp dice_id = posting_page.search('.company-header-info').css('[text()*="Dice Id"]').text.strip
+      puts
+      i+=1
+    end
 
-# # Fill in the field named "q" (Google's search query)
-# job_div.q = 'ruby mechanize'
+  end
 
-# # Actually submit the form
-# page = scraper.submit(job_div)
-
-# # Print out the page using the "pretty print" command
-# pp page
