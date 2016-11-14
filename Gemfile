ba0ab08d1bb202b06c089cc5847daf0e9ba556e8
Pramod Jacob <psjacob68@gmail.com>
2016-11-14 17:49:02 -0500

    broke up scraper.rb into classes

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ source "https://rubygems.org"
 # gem "rails"
   gem 'mechanize', '~>2.7.2', '<2.7.3'
   gem 'pry' # because you'll probably need it
+  gem 'nokogiri', '~>1.6.7.2'
--- scraper.rb
+++ scraper.rb
@@ -4,19 +4,57 @@ require 'mechanize'
 
 # create a scraper that performs a search query and returns the first results page
 
-scraper = Mechanize.new
+Job = Struct.new(:job_title, :company_name, :link, :location, :post_date, :company_id, :job_id)
 
-page = scraper.get('https://www.dice.com')
-form = page.form_with(:id => 'search-form')
-form.q = 'javascript'
-form.l = 'philadelphia' 
-page = scraper.submit(form)
+class Scraper
 
-array = page.links.select do |link|
-  link.text =~ /Developer/
+  def initialize(job, location)
+
+    raise ArgumentError unless job.is_a?(String) && location.is_a?(String)
+    scraper = Mechanize.new
+    url = build_url(job, location).new_url
+    page = scraper.get(url) 
+    pp page
+    scraper.history_added = Proc.new { sleep 0.5 }
+
+  end
+
+  def build_url(job, location) 
+
+    URLBuilder.new("https://www.dice.com/jobs?", q: job, l: location)
+
+  end
+
 end
 
-p array
+class URLBuilder 
 
-scraper.history_added = Proc.new { sleep 0.5 }
+  attr_reader :new_url 
+
+  def initialize(url, parameters = {}) 
+
+    @new_url = build_url(url, parameters)
+
+  end
+
+  def build_url(url, parameters)
+
+    string = url 
+    parameters.each do |key, value|
+      key = key.gsub(/\s/, "+")
+      value = value.gsub(/\s/, "+")
+      string += key.to_s + "=" + value + "&"
+    end
+    string
+
+  end
+
+end
+
+s = Scraper.new("developer", "philadelphia")
+
+# PSEUDOCODE
+
+# Find all items with class dice-btn-link 
+# Click on link for each 
 
