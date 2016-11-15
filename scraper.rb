664c6aa860c7cbbbd6d1bbc6da89fcdc1b0cc715
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-14 18:34:22 -0600

    Job struct

--- scraper.rb
+++ scraper.rb
@@ -8,36 +8,54 @@ Job = Struct.new(:title, :company_name, :link, :location, :post_date, :company_i
 
 class Scraper
 
-  attr_accessor :jobs
+  attr_reader :keyword, :location, :jobs
 
-  def initialize(job, location)
-    raise ArgumentError unless job.is_a?(String) && location.is_a?(String)
+  def initialize(keyword, location)
+    raise ArgumentError unless keyword.is_a?(String) && location.is_a?(String)
+    @keyword = keyword
+    @location = location
     @jobs = []
+    run
   end
 
   def run 
     scraper = Mechanize.new
-    url = build_url(job, location).new_url
+    url = build_url.new_url
     page = scraper.get(url)
 
     [page.links_with(:class => 'dice-btn-link')[12]].each do |link| ### TEMPORARY
       post_page = link.click # click on the link
-      @jobs << get_job(post_page)
+      @jobs << get_job(post_page, link)
     end
 
     scraper.history_added = Proc.new { sleep 0.5 }
   end
 
-  def build_url(job, location) 
-    URLBuilder.new("https://www.dice.com/jobs?", q: job, l: location)
+  def build_url 
+    URLBuilder.new("https://www.dice.com/jobs?", q: @keyword, l: @location)
   end
 
-  def get_job(post_page)
+  def get_job(post_page, link)
     job = Job.new
     job.title = post_page.search("h1.jobTitle").text
     job.company_name = post_page.search("li.employer a.dice-btn-link").text
-    # job.link = post_page.uri
-    job
+    job.link = link.href
+    job.location = post_page.search("li.location").text
+
+    date_regex = /\d*\-\d*\-\d*/
+    title = post_page.title
+    date = title.match(date_regex)[0]
+    job.post_date = date
+
+
+    dice_regex = /[^Dice Id : ]\d*/
+    dice_id = post_page.search('div.company-header-info div.col-md-12')[1].text   
+    job.company_id = dice_id.match(dice_regex)[0]
+
+    position_regex = /[^Position Id : ]\w*/
+    position_id = post_page.search('div.company-header-info div.col-md-12')[2].text 
+    job.id = position_id.match(position_regex)[0]
+
   end
 
 end
