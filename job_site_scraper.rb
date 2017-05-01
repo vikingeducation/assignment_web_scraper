9c7ece23dd5f2e3a291b0423fe62b6e2490086d2
Roy Chen <roychenlei@gmail.com>
2017-05-01 08:36:21 +0800

    added initial method to scrape job titles

--- job_site_scraper.rb
+++ job_site_scraper.rb
@@ -15,9 +15,21 @@ class JobSiteScraper
     # rate limit Mechanize instance
     self.agent.history_added = Proc.new { sleep 0.5 }
   end
+
+  def scrape_job_titles(page)
+    job_titles =  page.css("h2.jobtitle a")
+    job_titles.each do |job_title|
+      pp job_title.children.first.text
+    end
+  end
 end
 
 if $0 == __FILE__
   job_site_scraper = JobSiteScraper.new
   pp job_site_scraper.agent
+
+  # open page we want
+  page = job_site_scraper.agent.get('https://www.indeed.com.sg/jobs?q=ruby&l=Singapore')
+
+  job_site_scraper.scrape_job_titles(page)
 end
