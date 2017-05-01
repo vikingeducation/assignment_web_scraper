74146fe543cd3f27888aa870c757254b7d3b2082
Roy Chen <roychenlei@gmail.com>
2017-05-01 09:49:47 +0800

    modified comments

--- job_site_scraper.rb
+++ job_site_scraper.rb
@@ -14,21 +14,21 @@ class JobSiteScraper
       agent.user_agent_alias = 'Windows Chrome'
     end
 
-    # rate limit Mechanize instance
+    # rate limit Mechanize instance.
     self.agent.history_added = Proc.new { sleep 0.5 }
   end
 
-  # scrapes the page for each div that contains a job listing
+  # scrapes the page for each div that contains a job listing.
   def scrape_job_listings(page)
     job_listings = page.css("div.row.result")
   end
 
-  # parses a job listing for the job title
+  # parses a job listing for the job title.
   def scrape_job_title(listing)
     listing.css(".jobtitle").text.strip
   end
 
-  # parses a job listing for the hiring company
+  # parses a job listing for the hiring company.
   def scrape_company_name(listing)
     listing.css(".company").text.strip
   end
@@ -45,12 +45,11 @@ if $0 == __FILE__
   scraper = JobSiteScraper.new
   pp scraper.agent
 
-  # open page we want
+  # open page we want.
   page = scraper.agent.get('https://www.indeed.com.sg/jobs?q=ruby&l=Singapore')
 
   job_listings = scraper.scrape_job_listings(page)
 
-  # pp scraper.scrape_job_titles(job_listings.first)
   job_listings.each do |listing|
     pp scraper.scrape_job_title(listing)
     pp scraper.scrape_company_name(listing)
