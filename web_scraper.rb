64c870106d29af5d2f2b4cf5f882c67955d875fa
Chad Lucas <chad.lucas.19@gmail.com>
2017-05-06 01:03:29 -0500

    add location to job node

--- web_scraper.rb
+++ web_scraper.rb
@@ -21,18 +21,23 @@ agent.get(URL) do |search_page|
 	#submit form and puts results into variable
 	results_page = search_form.submit
 	#gets links for job titles
-	raw_links = results_page.search('h3')
+	raw_links = results_page.search('.serp-result-content')
 	#iterates through job titles and extracts text
+	#binding.pry
 	raw_links.each do |result|
-
-		title = result.search('a').text
+		#gets description
+		description = result.search('.shortdesc').text.strip
+		#gets title
+		title = result.search('h3 a').text.strip
+		#get location
+		location = result.search('.location').text
 		#puts job title into node
-		job_node = Job.new(title, nil, nil)
+		job_node = Job.new(title, location, description)
 		#iterates through node and prints data
 		job_node.each do
-
-			pp job_node.title.strip
-
+			puts
+			puts "title: #{job_node.title},\n :location: #{job_node.location},\n description: #{job_node.description}\n"
+			
 		end
 	end
 
