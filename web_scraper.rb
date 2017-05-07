e95f774e5fd0ab2a6d5a0513864ef7e905a26c41
andie5 <andieasa@googlemail.com>
2017-05-07 21:35:25 +0100

    scrape job listings and add them to csv file

--- web_scraper.rb
+++ web_scraper.rb
@@ -11,6 +11,7 @@ scraper = Mechanize.new
 
 # Grab and parse our page in one step
 page = scraper.get('http://uk.dice.com/')
+results = []
 
 dice_form = page.form
 # Enter the search terms and submit the form
@@ -28,27 +29,39 @@ scraper.history_added = Proc.new { sleep 0.5 }
 # Actually submit the form
 page = scraper.submit(dice_form, button)
 
+links = page.parser.css("h2.standardLink").children
+# this is the url
+# page = page.parser.css("h2.standardLink").children[0].attributes['id'].value
+
+# page = page.parser.css("h2.standardLink").children[0].attributes['href'].value
+
 page = page.parser.css("div#SearchResults").text.strip
+page = page.split("\"MESSAGE.ADVERT_SHORTLIST_COUNT_ALERT\" NOT FOUND\n\n\n\n\n\n\n\n\n\n\n")
 
-results = page.split("\"MESSAGE.ADVERT_SHORTLIST_COUNT_ALERT\" NOT FOUND\n\n\n\n\n\n\n\n\n\n\n")
-
-results.each do |job|
+i = 0
+page.each do |job|
   listing = Job.new
-  listing.title = job.scan((/\A(.*)\n\nSalary/).join.strip
+  listing.title = job.scan(/\A(.*)\n\nSalary/).join.strip
   listing.company = job.scan(/Advertiser\n\n(.*)/).join.strip
   listing.location = job.scan(/Location:\n(.*)/).join.strip
-  listing.post_date = job.scan(/Last Updated Date\n\n(.*)/)join.strip
-
+  listing.link = links[i].attributes['href'].value
+  listing.post_date = job.scan(/Last Updated Date\n\n(.*)/).join.strip
+  job_link = links[i].attributes['id'].value
+  listing.job_id = job_link.scan(/TITLE\[([0-9]+)\]/).join.strip
+  results << listing
+  i += 1
 end
+
+# Test code
 # page = page.at("div.searchResultTitle").text.strip
 
-# job_links = []
 
 # page.links_with(:href => /http:\/\/uk.dice.com\/IT-Job/).each do |link|
 #   job_links << link
 #   puts "This is a job link #{link}"
 # end
 
+# come back to this as this splits the result in a cleaner way
 # result = page.parser.css("div.searchResultTitle").text.strip.split("\n\n\n\n\n\n\n\n") 
 
 
@@ -59,10 +72,14 @@ end
 
 # it turns on Append Mode so you don't overwrite
 # your own scrape file
-# CSV.open('csv_file.csv', 'a') do |csv|
-#     # each one of these comes out in its own row.
-#     # csv << ['Harry', 'Potter', 'Wizard', '7/31/1980', 'Male', 'England']
-# end
+j = 0
+CSV.open('csv_file.csv', 'a') do |csv|
+  results.each do | item|
+    csv << item
+  end
+end
+
+
 
 # Print out the page using the "pretty print" command
-pp page
+pp results
